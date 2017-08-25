f0897eee6cfe244ef1ce5d7c4427e9e061b10101
Shinseiki86 <shinseiki86@gmail.com>
2017-08-25 01:16:14 -0500

    'widgets.forms.group' 'type'=>'select' permite selección múltiple.

---
+++ resources/views/cnfg-organizacionales/gerencias/form-inputs.blade.php
@@ -0,0 +1,11 @@
+{{--@include('datapicker')--}}
+@include('chosen')
+
+
+@include('widgets.forms.group', ['type'=>'text', 'name'=>'GERE_DESCRIPCION', 'label'=>'Descripción', 'options'=>['maxlength' => '100'] ])
+
+@include('widgets.forms.group', ['type'=>'select', 'name'=>'EMPL_ID', 'label'=>'Empresa', 'data'=>$arrEmpleadores])
+
+@include('widgets.forms.group', ['type'=>'select', 'name'=>'PROC_ids', 'label'=>'Procesos', 'data'=>$arrProcesos, 'multiple'=>true])
+
+@include('widgets.forms.group', [ 'type'=>'textarea', 'name'=>'GERE_OBSERVACIONES', 'label'=>'Observaciones', 'options'=>['maxlength' => '300'] ])
