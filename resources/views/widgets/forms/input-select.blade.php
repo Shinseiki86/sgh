98c8cb3452f000d00dcc40d236a706cd74620a89
ejsp81 <ejsp81@hotmail.com>
2017-09-01 12:18:40 -0500

    SELECT

--- resources/views/widgets/forms/input-select.blade.php
+++ resources/views/widgets/forms/input-select.blade.php
@@ -1,10 +1,21 @@
-{{ Form::select( isset($multiple) && $multiple ?$name.'[]':$name, (isset($multiple) && $multiple ? []:[null=>'']) + (isset($data)?$data:[]) , isset($value)? $value:old($name), ['id'=>$name, 'class'=>'form-control selectpicker'.(isset($readonly) && $readonly ?' readonly':''), 'data-size'=>5] + (isset($options)?$options:[]) + (isset($multiple) && $multiple ? ['multiple']:[]) ) }}
+{{ Form::select(
+	isset($multiple) && $multiple ?$name.'[]':$name,
+	(isset($multiple) && $multiple ? []:[''=>'']) + (isset($data)?$data:[]) , 
+	isset($value)? $value:old($name),
+	[
+		'id'=>$name,
+		'class'=>'form-control selectpicker'.(isset($readonly) && $readonly ?' readonly':''),
+		'data-size'=>5
+	] + (isset($options)?$options:[]) + (isset($multiple) && $multiple ? ['multiple']:[])
+) }}
 
+@if(is_array(old($name)) or isset(${$name}))
 @push('scripts')
 <script type="text/javascript">
 	$('#{{$name}}')
 		.val(
 			{{ old($name)!=null ?  json_encode(array_map('intval', old($name))) : (isset(${$name})?${$name}:'') }}
-		)//.selectpicker('render');
+		);//.selectpicker('render');
 </script>
 @endpush
+@endif
