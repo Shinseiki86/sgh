aa9c66dbc8bfae7921e2bd4d5d038a4645667679
Shinseiki86 <shinseiki86@gmail.com>
2017-09-06 00:05:51 -0500

    Cambios tickets/show.blade.php

--- resources/views/cnfg-tickets/tickets/show.blade.php
+++ resources/views/cnfg-tickets/tickets/show.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.menu')
 @section('title', '/ Detalle Ticket')
 
+@section('page_heading')
+	<div class="row">
+		<div id="titulo" class="col-xs-8 col-md-6 col-lg-6">
+			Ticket No. {{ str_pad($ticket->TICK_ID, 6, '0', STR_PAD_LEFT) }}
+		</div>
+		<div id="btns-top" class="col-xs-4 col-md-6 col-lg-6 text-right">
+			@rinclude('show-botones')
+		</div>
+	</div>
+@endsection
+
 @push('scripts')
 	<script type="text/javascript">
 		//===================================================================
 		//se encarga de mostrar la imagen y/o archivo
 		$("#imagenModal").on("click", function() {
 
-			var tickarchivo =  "{{ $ticket -> TICK_ARCHIVO }}";
-			var src = "{{asset('storages/' . $ticket -> TICK_ARCHIVO )}}";
+			var tickarchivo =  "{{ $ticket->TICK_ARCHIVO }}";
+			var src = "{{asset('storages/' . $ticket->TICK_ARCHIVO )}}";
 
 			if(tickarchivo == "" || tickarchivo == null){
 				src = "{{asset('storages/' . 'default.jpg' )}}";
@@ -28,7 +39,7 @@
 					$('#imagepreview').attr('src', src);
 					$('#imagemodal').modal('show'); 
 				}else{
-					src = "{{asset('storages/' . $ticket -> TICK_ARCHIVO )}}";
+					src = "{{asset('storages/' . $ticket->TICK_ARCHIVO )}}";
 					$('#imagepreview').attr('src', src); 
 					$('#imagemodal').modal('show'); 
 				}
@@ -55,7 +66,7 @@
 		//===================================================================
 		//si el estado de aprobación es diferente del inicial cuando se crea el ticket
 		//se esconden los botones
-		var ESAP_ID = "{{ $ticket -> ESAP_ID }}";
+		var ESAP_ID = "{{ $ticket->ESAP_ID }}";
 		
 		if(ESAP_ID != 1){
 			$('#daccionauto').hide();
@@ -76,166 +87,95 @@
 
 
 @section('section')
-	<h1 class="page-header">Ticket No. {{ str_pad($ticket->TICK_ID, 6, '0', STR_PAD_LEFT) }}:</h1>
 
-	<div class="jumbotron text-center">
-		<strong>Datos Generales</strong>
-		<p>
-			<ul class="list-group">
+	@section ('generales_title','Datos Generales')
+	@section ('generales_body')
+		<ul class="list-group">
+			@include('widgets.list-group-item', [
+				'label'=>'Empleador',
+				'value'=>$ticket->contrato->empleador->EMPL_RAZONSOCIAL
+			])
+			@include('widgets.list-group-item', [
+				'label'=>'Fecha Creación',
+				'value'=>$ticket->TICK_FECHASOLICITUD
+			])
+			@include('widgets.list-group-item', [
+				'label'=>'Empleado',
+				'value'=>nombre_empleado($ticket->contrato->PROS_ID)
+			])
+			@include('widgets.list-group-item', [
+				'label'=>'Estado',
+				'value'=>$ticket->estadoticket->ESTI_DESCRIPCION
+			])
+			@include('widgets.list-group-item', [
+				'label'=>'Tipo de Incidente',
+				'value'=>$ticket->tipoincidente->TIIN_DESCRIPCION
+			])
+			<li class="list-group-item">
+				<div class="row">
+					<div class="col-lg-4"><strong>Prioridad:</strong></div>
+					<div class="col-lg-8">{{ $ticket->prioridad->PRIO_DESCRIPCION }}</div>
+				</div>
+			</li>
 
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Empleador:</strong></div>
-						<div class="col-lg-8">{{ $ticket-> contrato -> empleador -> EMPL_RAZONSOCIAL }}</div>
-					</div>
-				</li>
+			<li class="list-group-item">
+				<div class="row">
+					<div class="col-lg-4"><strong>Categoría:</strong></div>
+					<div class="col-lg-8">{{ $ticket->categoria->CATE_DESCRIPCION }}</div>
+				</div>
+			</li>
 
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Fecha Creación:</strong></div>
-						<div class="col-lg-8">{{ $ticket->TICK_FECHASOLICITUD }}</div>
-					</div>
-				</li>
+			<li class="list-group-item">
+				<div class="row">
+					<div class="col-lg-4"><strong>Fecha del Evento:</strong></div>
+					<div class="col-lg-8">{{ $ticket->TICK_FECHAEVENTO }}</div>
+				</div>
+			</li>
 
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Empleado:</strong></div>
-						<div class="col-lg-8">{{ nombre_empleado($ticket -> contrato -> PROS_ID) }}</div>
-					</div>
-				</li>
+			<li class="list-group-item">
+				<div class="row">
+					<div class="col-lg-4"><strong>Observaciones:</strong></div>
+					<div class="col-lg-8">{{ $ticket->TICK_OBSERVACIONES }}</div>
+				</div>
+			</li>
 
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Estado:</strong></div>
-						<div class="col-lg-8">{{ $ticket -> estadoticket -> ESTI_DESCRIPCION }}</div>
-					</div>
-				</li>
+			<li class="list-group-item">
+				<div class="row">
+					<div class="col-lg-4"><strong>Estado Aprobación:</strong></div>
+					<div class="col-lg-8">{{ $ticket->estadoaprobacion->ESAP_DESCRIPCION }}</div>
+				</div>
+			</li>
+		</ul>
+	@endsection
+	@include('widgets.panel', ['as'=>'generales', 'header'=>true])
 
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Tipo de Incidente:</strong></div>
-						<div class="col-lg-8">{{ $ticket -> tipoincidente -> TIIN_DESCRIPCION }}</div>
-					</div>
-				</li>
+	@section ('hechos_title','Descripción de los Hechos')
+	@section ('hechos_body')
+		<div class="col-lg-8">{{ $ticket->TICK_DESCRIPCION }}</div>
+	@endsection
+	@include('widgets.panel', ['as'=>'hechos', 'header'=>true])
 
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Prioridad:</strong></div>
-						<div class="col-lg-8">{{ $ticket -> prioridad -> PRIO_DESCRIPCION }}</div>
-					</div>
-				</li>
-
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Categoría:</strong></div>
-						<div class="col-lg-8">{{ $ticket -> categoria -> CATE_DESCRIPCION }}</div>
-					</div>
-				</li>
-
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Fecha del Evento:</strong></div>
-						<div class="col-lg-8">{{ $ticket -> TICK_FECHAEVENTO }}</div>
-					</div>
-				</li>
-
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Observaciones:</strong></div>
-						<div class="col-lg-8">{{ $ticket -> TICK_OBSERVACIONES }}</div>
-					</div>
-				</li>
-
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Estado Aprobación:</strong></div>
-						<div class="col-lg-8">{{ $ticket -> estadoaprobacion -> ESAP_DESCRIPCION }}</div>
-					</div>
-				</li>
-
-
-			</ul>
-		</p>
-
-	</div>
-
-	<div class="jumbotron text-center">
-		<strong>Descripción de los Hechos</strong>
-		<p>
-			<ul class="list-group">
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Descripción de los Hechos:</strong></div>
-						<div class="col-lg-8">{{ $ticket -> TICK_DESCRIPCION }}</div>
-					</div>
-				</li>
-			</ul>
-		</p>
-
-	</div>
-
-	<div class="jumbotron text-center">
-		<strong>Evidencia</strong>
-		<p>
-			<ul class="list-group">
-				<li class="list-group-item">
-					<div class="row">
-						<div class="col-lg-4"><strong>Archivos:</strong></div>
-						<div class="col-lg-8">
-							<!-- Botón Ver (show) -->
-							<a class="btn btn-small btn-basic btn-xs" href="#" data-tooltip="tooltip" title="Ver" id="imagenModal">
-								<p hidden>
-									@if ($ticket -> TICK_ARCHIVO != NULL and $ticket -> TICK_ARCHIVO != "")
-									<img id="imageresource" src="{{asset('storages/' . $ticket -> TICK_ARCHIVO)}}" style="width: 500px; height: 264px;">
-									@else
-									<img id="imageresource" src="{{asset('storages/' . 'filedefault.png' )}}" style="width: 500px; height: 264px;">
-									@endif
-								</p>
-								<i class="fa fa-eye" aria-hidden="true"></i> Ver Archivo
-							</a>
-						</div>
-					</div>
-				</li>
-			</ul>
-		</p>
-	</div>
-
-	<div class="jumbotron text-center">
-		<strong>Acciones</strong>
-		<p>
-			<ul class="list-group">
-				<li class="list-group-item">
-					<div class="row" id="daccionauto">
-						<a href="#" class="btn btn-success" role="button" id="autorizarModal">
-							<span class="fa fa-check" aria-hidden="true"></span> Autorizar
-						</a>
-					</div>
-					<br>
-					<div class="row" id="daccionrecha">
-						<a href="#" class="btn btn-danger" role="button" id="rechazarModal">
-							<span class="fa fa-close" aria-hidden="true"></span> Rechazar
-						</a>
-					</div>
-					<div class="row" id="daccioncerrar">
-						<a href="#" class="btn btn-success" role="button" id="cerrarModal">
-							<span class="fa fa-check" aria-hidden="true"></span> Cerrar Ticket
-						</a>
-					</div>
-				</li>
-			</ul>
-		</p>
-	</div>
-
-	@rinclude('show-modales')
-
-	<!-- Botones -->
-	<div class="form-group">
-		<div class="col-md-6 col-md-offset-4 text-right">
-			<a class="btn btn-warning" role="button" href="{{ URL::to('cnfg-tickets/tickets/') }}" data-tooltip="tooltip" title="Regresar">
-				<i class="fa fa-arrow-left" aria-hidden="true"></i>
-			</a>
-
+	@section ('evidencias_title','Evidencia')
+	@section ('evidencias_body')
+		<div class="row">
+			<div class="col-lg-4"><strong>Archivos:</strong></div>
+			<div class="col-lg-8">
+				<!-- Botón Ver (show) -->
+				<a class="btn btn-small btn-basic btn-xs" href="#" data-tooltip="tooltip" title="Ver" id="imagenModal">
+					<p hidden>
+						@if ($ticket->TICK_ARCHIVO != NULL and $ticket->TICK_ARCHIVO != "")
+						<img id="imageresource" src="{{asset('storages/' . $ticket->TICK_ARCHIVO)}}" style="width: 500px; height: 264px;">
+						@else
+						<img id="imageresource" src="{{asset('storages/' . 'filedefault.png' )}}" style="width: 500px; height: 264px;">
+						@endif
+					</p>
+					<i class="fa fa-eye" aria-hidden="true"></i> Ver Archivo
+				</a>
+			</div>
 		</div>
-	</div>
+	@endsection
+	@include('widgets.panel', ['as'=>'evidencias', 'header'=>true])
 
+	{{-- @rinclude('show-botones') --}}
+	@rinclude('show-modales')
 @endsection
--- resources/views/widgets/panel.blade.php
+++ resources/views/widgets/panel.blade.php
@@ -1,7 +1,7 @@
 <div class="panel panel-{{{ isset($class) ? $class : 'default' }}}">
 	@if( isset($header) )  
 		<div class="panel-heading">
-			<h3 class="panel-title">@yield ($as . '_panel_title')
+			<h3 class="panel-title">@yield ($as . '_title')
 				@if( isset($controls) )  
 				<div class="panel-control pull-right">
 					<a class="panelButton"><i class="fa fa-refresh"></i></a>
@@ -14,10 +14,10 @@
 	@endif
 	
 	<div class="panel-body">
-		@yield ($as . '_panel_body')
+		@yield ($as . '_body')
 	</div>
 	@if( isset($footer))
-		<div class="panel-footer">@yield ($as . '_panel_footer')</div>
+		<div class="panel-footer">@yield ($as . '_footer')</div>
 	@endif
 </div>
 
