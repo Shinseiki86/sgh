aa9c1a7b7f123fa9d6689699acdff53c550ae33a
ejsp81 <ejsp81@hotmail.com>
2017-12-22 11:43:46 -0500

    correccion tildes

--- resources/views/widgets/rellena.blade.php
+++ resources/views/widgets/rellena.blade.php
@@ -1,8 +1,8 @@
 
 @push('scripts')  
   	<script>
-   		@foreach($columns as $col =>$value)	
-			$("#{{$col}}").val('{{ $value }}');
+   		@foreach($columns as $col =>$value)	   			
+			$("#{{$col}}").val('{!! str_replace('script', '', $value) !!}');	
 		@endforeach
 	</script> 
 @endpush
